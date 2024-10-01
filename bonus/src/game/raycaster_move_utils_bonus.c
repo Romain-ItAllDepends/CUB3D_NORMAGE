6c4123051b28ec1bf997c7c4997cba3fcab390e1
Intercalaire <vgodart@1F4.42angouleme.fr>
2024-10-01 13:24:09 +0200

    reglege du probleme collisison invisible

--- bonus/src/game/raycaster_move_utils_bonus.c
+++ bonus/src/game/raycaster_move_utils_bonus.c
@@ -15,39 +15,103 @@
 void	forward_and_back_utils_w(t_vars *vars, double moveSpeed)
 {
 	if (vars->map[(int)(vars->raycast->posx + vars->raycast->dirx
-			* MOVE_SPEED)][(int)vars->raycast->posy])
+			* MOVE_SPEED)][(int)vars->raycast->posy] == '0'
+			|| vars->map[(int)(vars->raycast->posx + vars->raycast->dirx
+			* MOVE_SPEED)][(int)vars->raycast->posy] == 'S'
+			|| vars->map[(int)(vars->raycast->posx + vars->raycast->dirx
+			* MOVE_SPEED)][(int)vars->raycast->posy] == 'N'
+			|| vars->map[(int)(vars->raycast->posx + vars->raycast->dirx
+			* MOVE_SPEED)][(int)vars->raycast->posy] == 'W'
+			|| vars->map[(int)(vars->raycast->posx + vars->raycast->dirx
+			* MOVE_SPEED)][(int)vars->raycast->posy] == 'E')
 		vars->raycast->posx += vars->raycast->dirx * moveSpeed;
 	if (vars->map[(int)(vars->raycast->posx)][(int)(vars->raycast->posy
-			+ vars->raycast->diry * MOVE_SPEED)])
+			+ vars->raycast->diry * MOVE_SPEED)] == '0'
+			|| vars->map[(int)(vars->raycast->posx)][(int)(vars->raycast->posy
+			+ vars->raycast->diry * MOVE_SPEED)] == 'S'
+			|| vars->map[(int)(vars->raycast->posx)][(int)(vars->raycast->posy
+			+ vars->raycast->diry * MOVE_SPEED)] == 'N'
+			|| vars->map[(int)(vars->raycast->posx)][(int)(vars->raycast->posy
+			+ vars->raycast->diry * MOVE_SPEED)] == 'W'
+			|| vars->map[(int)(vars->raycast->posx)][(int)(vars->raycast->posy
+			+ vars->raycast->diry * MOVE_SPEED)] == 'E')
 		vars->raycast->posy += vars->raycast->diry * moveSpeed;
 }
 
 void	forward_and_back_utils_s(t_vars *vars, double moveSpeed)
 {
 	if (vars->map[(int)(vars->raycast->posx - vars->raycast->dirx
-			* MOVE_SPEED)][(int)(vars->raycast->posy)])
+			* MOVE_SPEED)][(int)(vars->raycast->posy)] == '0'
+			|| vars->map[(int)(vars->raycast->posx - vars->raycast->dirx
+			* MOVE_SPEED)][(int)(vars->raycast->posy)] == 'S'
+			|| vars->map[(int)(vars->raycast->posx - vars->raycast->dirx
+			* MOVE_SPEED)][(int)(vars->raycast->posy)] == 'N'
+			|| vars->map[(int)(vars->raycast->posx - vars->raycast->dirx
+			* MOVE_SPEED)][(int)(vars->raycast->posy)] == 'W'
+			|| vars->map[(int)(vars->raycast->posx - vars->raycast->dirx
+			* MOVE_SPEED)][(int)(vars->raycast->posy)] == 'E')
 		vars->raycast->posx -= vars->raycast->dirx * moveSpeed;
 	if (vars->map[(int)(vars->raycast->posx)][(int)(vars->raycast->posy
-			- vars->raycast->diry * MOVE_SPEED)])
+			- vars->raycast->diry * MOVE_SPEED)] == '0'
+			|| vars->map[(int)(vars->raycast->posx)][(int)(vars->raycast->posy
+			- vars->raycast->diry * MOVE_SPEED)] == 'S'
+			|| vars->map[(int)(vars->raycast->posx)][(int)(vars->raycast->posy
+			- vars->raycast->diry * MOVE_SPEED)] == 'N'
+			|| vars->map[(int)(vars->raycast->posx)][(int)(vars->raycast->posy
+			- vars->raycast->diry * MOVE_SPEED)] == 'W'
+			|| vars->map[(int)(vars->raycast->posx)][(int)(vars->raycast->posy
+			- vars->raycast->diry * MOVE_SPEED)] == 'E')
 		vars->raycast->posy -= vars->raycast->diry * moveSpeed;
 }
 
 void	left_utils(t_vars *vars, double moveSpeed)
 {
 	if (vars->map[(int)(vars->raycast->posx - vars->raycast->planex
-			* MOVE_SPEED)][(int)vars->raycast->posy])
+			* MOVE_SPEED)][(int)vars->raycast->posy] == '0'
+			|| vars->map[(int)(vars->raycast->posx - vars->raycast->planex
+			* MOVE_SPEED)][(int)vars->raycast->posy] == 'S'
+			|| vars->map[(int)(vars->raycast->posx - vars->raycast->planex
+			* MOVE_SPEED)][(int)vars->raycast->posy] == 'N'
+			|| vars->map[(int)(vars->raycast->posx - vars->raycast->planex
+			* MOVE_SPEED)][(int)vars->raycast->posy] == 'W'
+			|| vars->map[(int)(vars->raycast->posx - vars->raycast->planex
+			* MOVE_SPEED)][(int)vars->raycast->posy] == 'E')
 		vars->raycast->posx -= vars->raycast->planex * moveSpeed;
 	if (vars->map[(int)(vars->raycast->posx)][(int)(vars->raycast->posy
-			- vars->raycast->planey * MOVE_SPEED)])
+			- vars->raycast->planey * MOVE_SPEED)] == '0'
+			|| vars->map[(int)(vars->raycast->posx)][(int)(vars->raycast->posy
+			- vars->raycast->planey * MOVE_SPEED)] == 'S'
+			|| vars->map[(int)(vars->raycast->posx)][(int)(vars->raycast->posy
+			- vars->raycast->planey * MOVE_SPEED)] == 'N'
+			|| vars->map[(int)(vars->raycast->posx)][(int)(vars->raycast->posy
+			- vars->raycast->planey * MOVE_SPEED)] == 'W'
+			|| vars->map[(int)(vars->raycast->posx)][(int)(vars->raycast->posy
+			- vars->raycast->planey * MOVE_SPEED)] == 'E')
 		vars->raycast->posy -= vars->raycast->planey * moveSpeed;
 }
 
 void	right_utils(t_vars *vars, double moveSpeed)
 {
 	if (vars->map[(int)(vars->raycast->posx + vars->raycast->planex
-			* MOVE_SPEED)][(int)vars->raycast->posy])
+			* MOVE_SPEED)][(int)vars->raycast->posy] == '0'
+			|| vars->map[(int)(vars->raycast->posx + vars->raycast->planex
+			* MOVE_SPEED)][(int)vars->raycast->posy] == 'S'
+			|| vars->map[(int)(vars->raycast->posx + vars->raycast->planex
+			* MOVE_SPEED)][(int)vars->raycast->posy] == 'N'
+			|| vars->map[(int)(vars->raycast->posx + vars->raycast->planex
+			* MOVE_SPEED)][(int)vars->raycast->posy] == 'W'
+			|| vars->map[(int)(vars->raycast->posx + vars->raycast->planex
+			* MOVE_SPEED)][(int)vars->raycast->posy] == 'E')
 		vars->raycast->posx += vars->raycast->planex * moveSpeed;
 	if (vars->map[(int)(vars->raycast->posx)][(int)(vars->raycast->posy
-			+ vars->raycast->planey * MOVE_SPEED)])
+			+ vars->raycast->planey * MOVE_SPEED)] == '0'
+			|| vars->map[(int)(vars->raycast->posx)][(int)(vars->raycast->posy
+			+ vars->raycast->planey * MOVE_SPEED)] == 'S'
+			|| vars->map[(int)(vars->raycast->posx)][(int)(vars->raycast->posy
+			+ vars->raycast->planey * MOVE_SPEED)] == 'N'
+			|| vars->map[(int)(vars->raycast->posx)][(int)(vars->raycast->posy
+			+ vars->raycast->planey * MOVE_SPEED)] == 'W'
+			|| vars->map[(int)(vars->raycast->posx)][(int)(vars->raycast->posy
+			+ vars->raycast->planey * MOVE_SPEED)] == 'E')
 		vars->raycast->posy += vars->raycast->planey * moveSpeed;
 }
